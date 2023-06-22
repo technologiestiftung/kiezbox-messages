0fc625aba4705515deb954a8df4c3d4a2f09b3fc
Matthieu Rigal <matthieu.rigal@ts.berlin>
2023-06-22 11:30:09 +0200

    Add support for 127.0.0.1 CORS origin

--- backend/main.py
+++ backend/main.py
@@ -20,6 +20,7 @@ extra_origin = os.getenv('UVICORN_HOST')
 # Enable CORS
 origins = [
     "http://localhost",
+    "http://127.0.0.1:8080",
     "http://localhost:8080",
     "http://localhost:3030"
 ]
