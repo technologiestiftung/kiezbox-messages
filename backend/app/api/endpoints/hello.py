1c2e419bd8eff3042bc66fb7ff691e788f52d72b
Matthieu Rigal <matthieu.rigal@ts.berlin>
2024-02-16 15:59:16 +0100

    feat: Rewrite using templates and sqlmodel

--- backend/app/api/endpoints/hello.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-
-router = APIRouter()
-
-@router.get("/")
-async def say_hello():
-    return {"message": "Hello World!"}
--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,38 @@
-from fastapi import FastAPI
+import asyncio
+from datetime import date
+import json
+import os
+from typing import List, Optional
+from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseSettings
+from fastapi import FastAPI, Query, Request
+from sqlalchemy import Column, DateTime, func
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sse_starlette import EventSourceResponse
 
-from app.api.api import api_router
-from app.core.config import settings
-from app.db.session import engine, Base
 
-Base.metadata.create_all(bind=engine)
+class Settings(BaseSettings):
+    API_STR: str = "/api"
+    PROJECT_NAME: str = "kiezbox-backend"
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./backend.db"
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
+extra_origin = os.getenv('UVICORN_HOST')
 
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_STR}/openapi.json"
 )
+app.mount("/static", StaticFiles(directory="static"))
+# app.add_middleware(GZipMiddleware)  # middleware except for SSE
+templates = Jinja2Templates(directory="templates")
 
 # Enable CORS
 origins = [
@@ -17,6 +40,9 @@ origins = [
     "http://localhost:8080",
     "http://localhost:3030"
 ]
+if extra_origin:
+    origins.append(extra_origin)
+    origins.append(f"{extra_origin}:8000")
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,4 +52,118 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(api_router, prefix=settings.API_STR)
+
+class Message(SQLModel, table=True):
+    
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    address: str
+    problem: str 
+    number_affected_ppl: str
+    timestamp: Optional[date] = Field(sa_column=Column(DateTime(timezone=True), index=True, server_default=func.now()))
+
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
+
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
+
+
+# Messages event generator TODO: Move to services directory
+STREAM_DELAY = 5  # In seconds TODO: Maybe move to settings
+# STREAM_TIMEOUT = 30000  # In milliseconds
+
+
+async def messages_event_generator(
+    request,
+    skip: int = 0,
+    limit: int = Query(default=100, le=100),
+    as_html: bool = False
+):
+    """
+    Get status as an event generator.
+    """
+    previous_messages = []
+    with Session(engine) as session:
+        while True:
+            # If client closes connection, stop sending events
+            if await request.is_disconnected():
+                break
+
+            # Check for new messages and return them to client if any
+            current_messages = session.exec(select(Message).order_by(Message.c.timestamp.desc()).offset(skip).limit(limit)).all()
+            if len(current_messages) > len(previous_messages):
+                if as_html:
+                    yield templates.TemplateResponse(request=request, name='inbox_content.html', context={'messages': current_messages})
+                else:
+                    yield json.dumps(jsonable_encoder(current_messages))
+
+                previous_messages = current_messages
+
+            await asyncio.sleep(STREAM_DELAY)
+
+
+@app.get("/api/messages/stream", response_model=List[Message])
+async def read_messages_stream(
+    request: Request,
+    skip: int = 0,
+    limit: int = Query(default=100, le=100)
+):
+    """
+    Retrieve messages and refresh with every new incoming message.
+    """
+    event_generator = messages_event_generator(request, skip=skip, limit=limit)
+
+    return EventSourceResponse(event_generator, send_timeout=5)
+
+
+@app.get("/api/messages/", response_model=List[Message])
+async def read_messages(skip: int = 0, limit: int = Query(default=100, le=100)):
+    with Session(engine) as session:
+        messages = session.exec(select(Message).order_by(Message.c.timestamp.desc()).offset(skip).limit(limit)).all()
+        return messages
+
+
+@app.post("/api/messages/", response_model=Message)
+async def send_message(message: Message):
+    with Session(engine) as session:
+        session.add(message)
+        session.commit()
+        session.refresh(message)
+        return message
+
+
+@app.get("/emergency", response_class=HTMLResponse)
+def get_emergency(request: Request):
+    return templates.TemplateResponse(request=request, name="emergency.html")
+
+
+@app.get("/inbox", response_class=HTMLResponse)
+def get_inbox(request: Request):
+    return templates.TemplateResponse(request=request, name="inbox.html")
+
+
+@app.get("/inbox/stream", response_model=List[Message])
+async def inbox_html_stream(
+    request: Request,
+    skip: int = 0,
+    limit: int = Query(default=100, le=100)
+):
+    """
+    Retrieve messages as HTML and refresh with every new incoming message.
+    """
+    event_generator = messages_event_generator(request, skip=skip, limit=limit, as_html=True)
+
+    return EventSourceResponse(event_generator, send_timeout=5)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def homepage(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
